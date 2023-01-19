d3bfe7f59c7fae31cc162c83b20d84d372598209
Muhitori <turevichkolya@gmail.com>
2023-01-19 11:31:40 +0200

    gender filter

---
+++ src/components/Home/GenderSelect.tsx
@@ -0,0 +1,38 @@
+import {
+	Box,
+	FormControl,
+	InputLabel,
+	Select,
+	MenuItem,
+	SelectChangeEvent,
+} from "@mui/material";
+import { FC } from "react";
+import { Gender } from "../../types/User";
+
+interface Props {
+	gender?: Gender;
+	onChange: (gender?: Gender) => void;
+}
+
+export const GenderSelect: FC<Props> = ({ gender, onChange }) => {
+	const handleChange = (event: SelectChangeEvent) => {
+		if (event.target.value !== "all") {
+			onChange(event.target.value as Gender | undefined);
+		} else {
+			onChange(undefined);
+		}
+	};
+
+	return (
+		<Box sx={{ width: 120 }}>
+			<FormControl fullWidth>
+				<InputLabel>Gender</InputLabel>
+				<Select value={gender} label='Gender' onChange={handleChange}>
+					<MenuItem value={"all"}>All</MenuItem>
+					<MenuItem value={Gender.MALE}>Male</MenuItem>
+					<MenuItem value={Gender.FEMALE}>Female</MenuItem>
+				</Select>
+			</FormControl>
+		</Box>
+	);
+};
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,36 +1,50 @@
 import { Container, Pagination } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { UserService } from "../../services/UserService";
-import { User } from "../../types/User";
+import { Gender, User } from "../../types/User";
 import { columns } from "./columns";
+import { GenderSelect } from "./GenderSelect";
 
 export const Home = () => {
 	const [rows, setRows] = useState<User[]>([]);
+
+	const [gender, setGender] = useState<Gender | undefined>();
 
 	const [currentPage, setCurrentPage] = useState<number>(1);
 	const [pageCount, setPageCount] = useState<number | undefined>();
 
 	useEffect(() => {
 		const effect = async () => {
-			const { data, pageCount } = await UserService.getUsers(currentPage);
+			const { data, pageCount } = await UserService.getUsers(
+				currentPage,
+				gender
+			);
 
 			setRows(data);
 			setPageCount(pageCount);
 		};
 
 		effect();
-	}, [currentPage]);
+	}, [currentPage, gender]);
 
-	const handlePageChange = (
-		event: React.ChangeEvent<unknown>,
-		value: number
-	) => {
-		setCurrentPage(value);
-	};
+	const handleGenderChange = useCallback(
+		(gender: Gender | undefined) => {
+			setGender(gender);
+		},
+		[setGender]
+	);
+
+	const handlePageChange = useCallback(
+		(event: React.ChangeEvent<unknown>, value: number) => {
+			setCurrentPage(value);
+		},
+		[setCurrentPage]
+	);
 
 	return (
-		<Container sx={{ py: 2 }}>
+		<Container sx={{ display: "flex", flexDirection: "column", gap: 2, pt: 2 }}>
+			<GenderSelect gender={gender} onChange={handleGenderChange} />
 			{Boolean(rows.length) && (
 				<DataGrid
 					autoHeight
